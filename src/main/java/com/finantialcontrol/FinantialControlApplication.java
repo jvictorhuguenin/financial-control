73c247c52775a4c1a04df9181b02946ac072a16d
jvictorhuguenin <j.victorhuguenin2018@gmail.com>
2022-06-18 19:50:40 -0300

    Firts commit

---
+++ src/main/java/com/finantialcontrol/FinantialControlApplication.java
@@ -0,0 +1,13 @@
+package com.finantialcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinantialControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinantialControlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/finantialcontrol/FinantialControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finantialcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinantialControlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
